3b1da97db7b7e9382344c005aab09ed98996b556
chetanahire <chetanahire01.429@gmail.com>
2021-05-04 00:43:17 +0530

    initial commit

---
+++ src/main/java/com/cs/JavaUtilityWithS3Bucket.java
@@ -0,0 +1,7 @@
+package com.cs;
+
+public class JavaUtilityWithS3Bucket {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/cs/JavaUtilityWithS3BucketTest.java
@@ -0,0 +1,12 @@
+package com.cs;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JavaUtilityWithS3BucketTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
